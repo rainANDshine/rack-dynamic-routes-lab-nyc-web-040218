182edbd1c683f26d4bd8bd35a838b2a7e2e884c5
Shun Yao <ysmiracle@hotmail.com>
2018-04-23 01:32:44 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,22 @@
+class Application
+  @@items = []
+ 
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path == "/items"
+      #if req.path.match(/items/)
+   
+      item_title = req.path.split("/items/").last
+      item = @@items.find{|i| i.name == item_title}
+      resp.write item || item.price
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+ 
+    resp.finish
+  end
+end
